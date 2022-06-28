ff3ae3342ce996dcc767eff626b4a1744ce1ea9d
Ann <ania.somko@gmail.com>
2022-06-28 11:08:16 +0300

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
-//import Notiflix from 'notiflix';
+import { Notify } from 'notiflix';
 import API from './fetchCountries.js';
 const DEBOUNCE_DELAY = 300;
 
@@ -12,21 +12,50 @@ formInput.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
 function onInputSearch(e) {
   let name = formInput.value;
   API.fetchCountries(name)
-    .then(countries => renderCountryCard(countries))
-    .catch(error => console.log(error));
+    .then(countries => {
+      if (countries.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+
+        return;
+      }
+      if (countries.length > 1) {
+        renderCountries((countries.length = 10));
+        return;
+      }
+      if (countries.length == 1) {
+        renderCountry(countries[0]);
+      }
+    })
+    .catch(Notify.failure('Oops, there is no country with that name'));
 }
 
-function renderCountryCard(countries) {
+function renderCountries(countries) {
   const markup = countries
     .map(country => {
       return `
       <li class="list_item"><img src="${country.flags.png}" width="30" height="20"></li>
       <li class="list_item"><h2>${country.name}</h2></li>
-          <li><b>Capital:</b> ${country.capital}</li>
-          <li><b>Population:</b> ${country.population}</li>
-          <li><b>Languages:</b> ${country.languages[0].name}</li>
         `;
     })
     .join('');
   countryList.innerHTML = markup;
 }
+
+function renderCountry(countries) {
+  const markup = countries
+    .map(country => {
+      return `
+    <li class="list_item"><img src="${country.flags.png}" width="30" height="20"></li>
+    <li class="list_item"><h2>${country.name}</h2></li>
+        <li><b>Capital:</b> ${country.capital}</li>
+        <li><b>Population:</b> ${country.population}</li>
+        <li><b>Languages:</b> ${country.languages[0].name}</li>
+      `;
+    })
+    .join('');
+  countryList.innerHTML = markup;
+}
+
+//.filter(el => el.name <= 10)
