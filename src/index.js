ce1964287a6b4ad48a5f06e0b4366776377770c5
Ann <ania.somko@gmail.com>
2022-06-28 16:31:20 +0300

    change task

--- src/index.js
+++ src/index.js
@@ -11,51 +11,51 @@ formInput.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
 
 function onInputSearch(e) {
   let name = formInput.value;
-  API.fetchCountries(name)
+  let trimmedName = name.trim();
+  API.fetchCountries(trimmedName)
     .then(countries => {
       if (countries.length > 10) {
         Notify.info(
           'Too many matches found. Please enter a more specific name.'
         );
-
         return;
       }
-      if (countries.length > 1) {
-        renderCountries((countries.length = 10));
+      if (countries.length <= 10) {
+        renderCountries(countries);
         return;
       }
       if (countries.length == 1) {
-        renderCountry(countries[0]);
+        renderOneCountry(countries[0]);
       }
     })
     .catch(Notify.failure('Oops, there is no country with that name'));
 }
 
 function renderCountries(countries) {
-  const markup = countries
+  let markup = countries
     .map(country => {
       return `
-      <li class="list_item"><img src="${country.flags.png}" width="30" height="20"></li>
-      <li class="list_item"><h2>${country.name}</h2></li>
-        `;
+      <li>
+        <li class="list_item"><img src="${country.flags.png}" width="30" height="20"></li> 
+        <li class="list_item"><h2>${country.name}</h2></li>
+      </li> `;
     })
     .join('');
   countryList.innerHTML = markup;
 }
 
-function renderCountry(countries) {
-  const markup = countries
-    .map(country => {
-      return `
-    <li class="list_item"><img src="${country.flags.png}" width="30" height="20"></li>
-    <li class="list_item"><h2>${country.name}</h2></li>
-        <li><b>Capital:</b> ${country.capital}</li>
-        <li><b>Population:</b> ${country.population}</li>
-        <li><b>Languages:</b> ${country.languages[0].name}</li>
+function renderOneCountry(country) {
+  countryList.innerHTML = `
+    <li>
+      <li class="list_item"><img src="${
+        country.flags.png
+      }" width="30" height="20"></li>
+      <li class="list_item"><h2>${country.name}</h2></li>
+    </li>
+    <li><b>Capital:</b> ${country.capital}</li>
+    <li><b>Population:</b> ${country.population}</li>
+    <li><b>Languages:</b> ${Object.values(country.languages).join(', ')}</li>
       `;
-    })
-    .join('');
-  countryList.innerHTML = markup;
 }
 
 //.filter(el => el.name <= 10)
